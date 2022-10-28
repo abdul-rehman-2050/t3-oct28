ad5f262f10790f0ec4974607616c0c37a399591c
Abdul Rehman <supermind2002@gmail.com>
2022-10-28 21:02:53 +0500

    one error

--- src/layouts/dashboard1-layout.tsx
+++ src/layouts/dashboard1-layout.tsx
@@ -3,15 +3,16 @@ import { IChildren } from "../types/global";
 import Sidebar1 from "../components/sidebar1";
 import Appbar from "../components/appbar";
 
+
 function Dashboar1Layout({ children }: IChildren) {
   return (
     <>
       <Appbar />
       <div className="flex bg-slate-500">
-        <div className="w-64 hidden md:block ">
+        <div className="hidden w-64 md:block ">
           <Sidebar1 />
         </div>
-        <div className="flex-row w-full">{children}</div>
+        <div className="w-full flex-row p-8">{children}</div>
       </div>
     </>
   );
--- src/pages/test1.tsx
+++ src/pages/test1.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Dashboar1Layout from "../layouts/dashboard1-layout";
 
+
 function Test1() {
-  return (
-    <Dashboar1Layout>
-      <div className="h-64 w-64 bg-teal-500 p-10">
-        what is thasdfasdfasdfasdfis
-      </div>
-      <div className="h-screen w-full bg-teal-50 p-10">
-        what is thasdfasdfasdfasdfis
-      </div>
-    </Dashboar1Layout>
+    const rows [];
+for (let i = 0; i < 10; i++) {
+  rows.push(
+    <div className="h-64 w-64 bg-teal-500 p-10 shadow-md shadow-white">
+      what is thasdfasdfasdfasdfis
+    </div>
   );
+}
+
+  return <Dashboar1Layout>{rows}</Dashboar1Layout>;
 }
 
 export default Test1;
