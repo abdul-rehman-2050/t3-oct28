737b5966ba89a75ef217363cb5f2944a01eb3b54
abdul rehman 2050 <abdul.rehman.vud@gmail.com>
2022-10-29 11:39:59 -0700

    react-toastify added

--- src/pages/examples/test3.tsx
+++ src/pages/examples/test3.tsx
@@ -1,17 +1,33 @@
 import React from "react";
 import Dashboar1Layout from "../../layouts/dashboard1-layout";
 import FlexCardsMultiple from "../../examples/tailwind-examples/flexcards-multiple-ex01";
-import TopAlertEx01 from "../../examples/tailwind-examples/topalert-ex01";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+
 function Test3() {
+  const notify = () => toast.info("Wow so easy!");
   return (
     <Dashboar1Layout bgcolor="bg-gray-400">
       <div>
-        <div className="w-full">
-            <TopAlertEx01 message="something" type="bg-bs-success"/>
-        </div>
+        <button onClick={notify}>notify</button>
         <div className="flex flex-wrap">
           <FlexCardsMultiple cardnum={5} />
         </div>
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+
+        />
       </div>
     </Dashboar1Layout>
   );
