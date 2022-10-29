9d59c5b145fe3f558ef2a7e77a243eb55bcfde81
Abdul Rehman <supermind2002@gmail.com>
2022-10-29 18:06:57 +0500

    alert componenet ex

--- src/pages/examples/test3.tsx
+++ src/pages/examples/test3.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 import Dashboar1Layout from "../../layouts/dashboard1-layout";
 import FlexCardsMultiple from "../../examples/tailwind-examples/flexcards-multiple-ex01";
-
+import TopAlertEx01 from "../../examples/tailwind-examples/topalert-ex01";
 function Test3() {
-  
-  
-
   return (
-    <Dashboar1Layout bgcolor="bg-gray-400">      
-        <div className="flex flex-wrap">
-            <FlexCardsMultiple cardnum={5}/>
+    <Dashboar1Layout bgcolor="bg-gray-400">
+      <div>
+        <div className="w-full">
+            <TopAlertEx01 message="something" type="bg-bs-success"/>
         </div>
-        
+        <div className="flex flex-wrap">
+          <FlexCardsMultiple cardnum={5} />
+        </div>
+      </div>
     </Dashboar1Layout>
   );
 }
