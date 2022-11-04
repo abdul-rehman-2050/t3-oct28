6990f34a0a11a50aba8ae497f5be7c8832cae87a
Abdul Rehman <supermind2002@gmail.com>
2022-11-04 20:16:34 +0500

    action buttons added

--- src/components/users/user-info-card.tsx
+++ src/components/users/user-info-card.tsx
@@ -1,22 +1,23 @@
 import Link from "next/link";
 import React from "react";
 import { IFakeUser } from "../../server/trpc/router/faker";
+import Image from "next/image";
 
 function UserInfoCard(props: IFakeUser) {
-    console.log(props.image)
+  console.log(props.image);
 
   return (
     <div className="right-0 mx-auto mt-2 w-60">
       <div className="overflow-hidden rounded bg-white shadow-lg">
         <div className="border-b bg-gray-800 p-6 text-center">
           {props.image ? (
-            <img
+            <Image
               src={props.image}
               alt="user"
               className="mx-auto h-24 w-24 rounded-full text-white"
-              width="32"
-              height="32"
-            ></img>
+              width="132"
+              height="132"
+            />
           ) : (
             <svg
               aria-hidden="true"
@@ -37,7 +38,7 @@ function UserInfoCard(props: IFakeUser) {
           <p className="text-sm text-gray-100">{props.email}</p>
           <div className="mt-5">
             <a className="rounded-full border py-2 px-4 text-xs font-semibold text-gray-100">
-              Manage your Account
+              Manage Account
             </a>
           </div>
         </div>
@@ -92,26 +93,12 @@ function UserInfoCard(props: IFakeUser) {
           </Link>
         </div>
 
-        <div className="">
-          <button className="flex w-full px-4 py-2 pb-4 hover:bg-gray-100">
-            <p className="text-sm font-medium leading-none text-gray-800">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                aria-hidden="true"
-                role="img"
-                className="h-4 w-4 animate-spin fill-current text-gray-800"
-                width="32"
-                height="32"
-                preserveAspectRatio="xMidYMid meet"
-                viewBox="0 0 1024 1024"
-              >
-                <path
-                  fill="currentColor"
-                  d="M988 548c-19.9 0-36-16.1-36-36c0-59.4-11.6-117-34.6-171.3a440.45 440.45 0 0 0-94.3-139.9a437.71 437.71 0 0 0-139.9-94.3C629 83.6 571.4 72 512 72c-19.9 0-36-16.1-36-36s16.1-36 36-36c69.1 0 136.2 13.5 199.3 40.3C772.3 66 827 103 874 150c47 47 83.9 101.8 109.7 162.7c26.7 63.1 40.2 130.2 40.2 199.3c.1 19.9-16 36-35.9 36z"
-                ></path>
-              </svg>{" "}
-              Logout
-            </p>
+        <div className="flex btn-group justify-center">
+          <button className="btn btn-outline btn-success">
+            Logout
+          </button>
+          <button className="btn btn-outline btn-error ">
+           Delete
           </button>
         </div>
       </div>
